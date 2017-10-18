a031fa2ed12636a553c4461984c460dbad9f01f1
janda <158464459@qq.com>
2017-10-18 12:45:55 +0800

    添加菜单

--- frontend/src/components/index.vue
+++ frontend/src/components/index.vue
@@ -187,6 +187,23 @@
               <span class="ch">page2</span>
             </router-link>
           </li>
+          <li class="treeview">
+            <a href="#">
+              <i class="fa fa-edit"></i> <span>Forms</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+              <li>
+                <router-link to="/general">
+                  <a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> General Elements</a>
+                </router-link>
+              </li>
+              <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
+              <li><a href="pages/forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
+            </ul>
+        </li>
         </ul>
       </section>
     </aside>
