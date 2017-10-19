2333290420dcbef611d4681d085dced0ecd1fa73
jared <liu54673@qq.com>
2017-10-19 09:13:44 +0800

    first blood

--- backend/src/main/java/com/liu/HelloController.java
+++ backend/src/main/java/com/liu/HelloController.java
@@ -3,6 +3,9 @@ package com.liu;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author jared
+ */
 @RestController
 @RequestMapping("/api")
 public class HelloController {
