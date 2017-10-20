7335f16c806b13d04e732886d4df7bc31b654968
janda <158464459@qq.com>
2017-10-20 16:30:45 +0800

    eslint 语法校验关闭

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -37,6 +37,7 @@ module.exports = {
   module: {
     rules: [
       {
+        /*
         test: /\.(js|vue)$/,
         loader: 'eslint-loader',
         enforce: 'pre',
@@ -44,6 +45,7 @@ module.exports = {
         options: {
           formatter: require('eslint-friendly-formatter')
         }
+        */
       },
       {
         test: /\.vue$/,
