84c9a2667279db50b7d4b948e41f1db044ba6120
janda <158464459@qq.com>
2017-10-20 18:03:54 +0800

    火车票信息

--- frontend/src/components/index.vue
+++ frontend/src/components/index.vue
@@ -231,6 +231,14 @@
                   </span>
                 </router-link>
               </li>
+              <li>
+                <router-link to="/ticketInfo">
+                  <i class="fa fa-th"></i> <span>火车票信息</span>
+                  <span class="pull-right-container">
+                    <small class="label pull-right bg-green">Hot</small>
+                  </span>
+                </router-link>
+              </li>
               <li class="header">LABELS</li>
               <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
               <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
