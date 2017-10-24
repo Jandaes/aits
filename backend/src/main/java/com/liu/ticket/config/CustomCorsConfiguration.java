76f082893960c2b13815ac3c41939815ff955b58
janda <158464459@qq.com>
2017-10-24 10:08:11 +0800

    全局添加 Cors

--- backend/src/main/java/com/liu/ticket/config/CustomCorsConfiguration.java
+++ backend/src/main/java/com/liu/ticket/config/CustomCorsConfiguration.java
@@ -17,7 +17,7 @@ public class CustomCorsConfiguration {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/ticketInfo/**").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
