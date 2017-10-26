1642b6bf62584882c9b2c22a55f17d29f9683f0e
janda <158464459@qq.com>
2017-10-26 16:10:40 +0800

    vue的this、js的this冲突

--- frontend/src/components/catalog2.vue
+++ frontend/src/components/catalog2.vue
@@ -9,10 +9,10 @@
           ========================
         </div>
         <div id="conversationDiv">
-          <label>输入你的名字</label><input type="text" id="name" />
+          <label>输入你的名字</label><input type="text" v-model="name"/>
           <button id="connect" @click="connect">连接</button>
           <button id="sendName" @click="sendName">发送</button>
-          <p id="response"></p>
+          <p>{{ response }}</p>
         </div>
       </div>
   </body>
@@ -22,27 +22,33 @@
   var stompClient=null;
   var message=null;
   export default {
+    data() {
+      return {
+        name:'',
+        response:''
+      }
+    },
     created(){
       this.disconnect();
     },
     methods: {
       setConnected: function (connected) {
-        $('#response').html();
+        this.response="";
       },
       connect: function () {
+        //js的this和vue的this冲突
+        var _this = this;
         //连接sockjs的endpoint名称为/endPointJared
         var socket=new SockJS('http://localhost:8999/endPointJared');
         //使用stomp子协议的websocket客户端
         stompClient=Stomp.over(socket);
         //连接websocket服务端
         stompClient.connect({},function (frame) {
-         // this.setConnected(true);
+          // this.setConnected(true);
           console.log("connected:"+frame);
           //通过stompclient.send向/welcome目标（destination）发送消息，这个是在控制器的@messageMapping中定义的
           stompClient.subscribe("/topic/getResponse",function (response) {
-            var message=JSON.parse(response.body).responseMessage;
-            var response=  $("#response");
-            response.html(message);
+            _this.response = JSON.parse(response.body).responseMessage;
           });
         });
       },
@@ -50,14 +56,12 @@
         if(stompClient!=null){
           stompClient.disconnect();
         }
-           this.setConnected(false);
+        this.setConnected(false);
         console.log("Disconnected");
       },
       sendName: function () {
-        var name=$('#name').val();
-        stompClient.send("/welcome",{},JSON.stringify({'name':name}));
+        stompClient.send("/welcome",{},JSON.stringify({'name':this.name}));
       }
-
     }
   }
 </script>
