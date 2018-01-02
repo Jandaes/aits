125bde0ac0613449f49d3780178f64ff293e8955
jared <liu54673@msn.cn>
2018-01-02 11:44:50 +0800

    加入文件管理机制

--- aits_app1/src/main/java/com/jared/aits_app1/AitsApp1Application.java
+++ aits_app1/src/main/java/com/jared/aits_app1/AitsApp1Application.java
@@ -2,8 +2,10 @@ package com.jared.aits_app1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class AitsApp1Application {
 
 	public static void main(String[] args) {
